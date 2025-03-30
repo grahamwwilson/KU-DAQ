50fee0c3954ca097100898e59a201d9b503a4571
graham <Graham.Wilson@cern.ch>
2025-03-29 21:05:04 -0500

    Add Simple_VME example

---
+++ Simple_VME/Simple_VME.cpp
@@ -0,0 +1,107 @@
+/* Simple VM_USB program
+ * 
+ * Copyright (C) 2005-2009 WIENER, Plein & Baus, Ltd (thoagland@wiener-us.com)
+ *
+ *     This program is free software; you can redistribute it and/or
+ *     modify it under the terms of the GNU General Public License as
+ *     published by the Free Software Foundation, version 2.
+ *
+ *
+ *
+ * This file will show the basic principles of using the xx_usb
+ * Library.  It is offerered as a simple version idea of how to:
+ *       Open the Device
+ *       Set up the LEDs
+ *       Set up a DGG
+ *       Close the Device
+ * In order for it to work be sure to change the serial number
+ * To that of your VM_USB.
+ *
+ *
+ * To compile, I have assumed that you have already put libxx_usb.so
+ * and xx_usb.h in a standard location 
+ * compile as:
+ * gcc -shared Simple_VME Simple_VME.c -lxx_usb
+ */
+
+// Started here from example for XX-USB_CD2_0
+// and updated to                xxusb_3.13 
+// which has a new library etc.
+
+// Updated here to xxusb_3.3 and had to do major revisions to get it to work with libusb-1.0.
+// Graham  March 29th, 2025.
+// Current compilation command that produces an executable is 
+// g++ -v -O -Wall -fPIC -g -I/usr/include/libusb-1.0/ -L/usr/lib/ -o Simple_VME.exe Simple_VME.cpp -lxx_usb -lusb-1.0 -Wl,-rpath=/usr/lib/
+//
+// Now have simplified the Makefile, and it implements the following.
+// g++ -v -O -Wall -fPIC -g -I/usr/include/libusb-1.0/ -o Simple_VME.exe Simple_VME.cpp -lxx_usb -lusb-1.0 -L/usr/lib/
+// May also need to export LD_LIBRARY_PATH=/usr/lib:$LD_LIBRARY_PATH although I don't think so.
+
+//#include <xx_usb.h>
+#include <stdio.h>
+#include <libxxusb.h>
+#include <unistd.h>  // For sleep()
+#include <cstring>
+
+int main ()
+{
+
+// New C/C++ standards dont-like mutable character array but that is used in the interface.
+
+// Wiener VM-USB received Summer 2006
+    const char* constString = "VM0036";    // The one with the VHQ
+// The second Wiener VM-USB received 2007?
+//    const char* constString = "VM0064";    // The one with the V560N scaler    
+
+// Allocate memory for the modifiable char array
+    char* serialString =  new char[strlen(constString) + 1];  // +1 for null terminator. This is the serial number of VM_USB 
+
+// Copy the content of the const string to the modifiable one
+    strcpy(serialString, constString);
+
+    libusb_device_handle *udev;       // Device Handle     
+
+    //GWW Use code as in vmusb_flash.cpp
+    long fwid;
+    int ret;
+
+    udev = xxusb_serial_open(serialString);  // Open Device and get handle
+    
+    // Make sure VM_USB opened OK
+    if(!udev) 
+    {
+	printf ("\nFailed to open VM_USB \n\n");
+	
+	delete[] serialString;
+
+	return 0;
+    }
+    else{
+	printf ("\nEstablished communication with VM_USB \n\n");        
+    }
+
+    // Check the firmware version
+    printf ("\n\nChecking the programmed firmware version\n\n");
+
+    sleep(1);
+
+    ret=VME_register_read(udev,0,&fwid);
+    printf("\nThe return code is %dx\n\n",ret);
+    printf("\nThe Firmware ID is %lx\n\n\n",fwid);
+
+    // Set Red LED to light with NIM1
+    printf ("\nCalling VME_LED_settings\n\n");
+    VME_LED_settings(udev,1,1,0,0);   
+    printf ("\nCalled VME_LED_settings\n\n");
+
+    // Set DGG channel A to trigger on NIM1, output on O1,
+    //     with delay =10 x 12.5ns,
+    //     and width =  20 x 12.5ns,
+    //     not latching or inverting
+    printf ("\nCalling VME_DGG\n\n");
+    VME_DGG(udev,0,1,0,10,20,0,0);
+    printf ("\nCalled VME_DGG\n\n");
+    
+    // Close the Device
+    xxusb_device_close(udev);
+}
